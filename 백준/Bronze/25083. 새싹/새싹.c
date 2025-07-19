dcc8df8c0abb0da9b7fe34b5d1360f6092b4c897
nasi546 <alstjd546@naver.com>
2025-07-19 15:19:22 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
+#include <time.h>
+int main()
+{
+printf("         ,r'\"7\n");
+printf("r`-_   ,'  ,/\n");
+printf(" \\. \". L_r'\n");
+printf("   `~\\/\n");
+printf("      |\n");
+printf("      |\n");
+
+
+
+
+
+	return 0;
+}
