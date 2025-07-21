53f2b541e037318133dcfcec1f1b62d0e723842b
nasi546 <alstjd546@naver.com>
2025-07-22 08:30:38 +0900

    [Silver V] Title: 너의 평점은, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.c
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.c
@@ -1,35 +1,46 @@
 #include <stdio.h>
 #include <string.h>
 
-double point(const char* grade) {
-    const char* grades[] = {"A+", "A0", "B+", "B0", "C+", "C0", "D+", "D0", "F"};
-    const double scores[] = {4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0.0};
+double point(const char* grade)
+{
+	const char* grades[] = {"A+", "A0", "B+", "B0", "C+", "C0", "D+", "D0", "F"};
+	const double scores[] = {4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0.0};
 
-    for (int i = 0; i < 9; i++) {
-        if (strcmp(grade, grades[i]) == 0)
-            return scores[i];
-    }
-    return 0.0;
+	
+	for (int i = 0; i < 9; i++)
+	{
+		if (strcmp(grade, grades[i]) == 0)
+		{
+			return scores[i];
+		}
+	}
+
+	return 0.0;
 }
 
-int main() {
-    char subject[100];
-    char grade[3];
-    double score;
-    double total = 0.0;
-    double credit_sum = 0.0;
+int main()
+{
+	double total = 0.0;
+	char grade[3];
+	char dummy[100];
+	double score;
+	double totalexam =0.0;
+	for (int i = 0; i < 20; i++)
+	{
+		scanf("%s %lf %s",dummy, &score, grade);
+		if (strcmp(grade, "P") == 0)
+		{
+			continue;
+		}
 
-    for (int i = 0; i < 20; i++) {
-        scanf("%s %lf %s", subject, &score, grade);
+		total += score * point(grade);
+		totalexam += score;
+	}
 
-        if (strcmp(grade, "P") == 0) {
-            continue;  // P는 계산에서 제외
-        }
 
-        total += score * point(grade);
-        credit_sum += score;
-    }
+	
 
-    printf("%.6lf\n", total / credit_sum);
-    return 0;
+	printf("%.6lf\n", total/totalexam);
+return 0;
 }
+
